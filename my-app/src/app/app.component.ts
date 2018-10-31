0f52d81fcbf6e0449e1519920b8cda2e6d1c4b44
John Lundeen <lunde367@umn.edu>
2018-10-31 14:17:47 -0500

    add some initial styles to the app

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -8,3 +8,39 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-app';
 }
+
+
+/*
+
+Important Links:
+  - Bootstrap 4 Components: https://getbootstrap.com/docs/4.1/components
+
+TODO:
+- need a way to add/delete friends
+- need a way to enter stocks bought
+- need a way to enter need way to enter money per user
+- need a way to take out available money per user
+- need a way to vote on which stocks to buy
+- need a way to log in
+- need a way to logout
+- need a way to create a stock club (initial setup)
+
+
+- Initial screen: 
+  - login
+  - create new account
+
+- After login: 
+  - bring to dashboard
+  - be able to add/delete stock club members (make it invite only to start)
+  - be able to view money values (total, used, available, pending, growth/loss, etc) (stock club specific)
+  - be able to enter more money (user specific)
+  - be able to take out available money (user specific)
+  - be able to propose a trade to stock club (question: what is the limit for trade proposals? (ex: 1 a day per group, infinite, etc.))
+  - be able to vote on a proposed trade (default: need 100% yes to make trade)
+  - be able to create new stock club (make it so you can only do this if you aren't in a stock club to start)
+
+  - Creating a new stock club:
+  - name club
+  - add initial members 
+*/
