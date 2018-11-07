93d0e7695d88da788a0f5cecb01685fed74f0815
johnisawesome94 <lunde367@umn.edu>
2018-11-07 14:52:04 -0600

    update mongo creds

--- stock-club-api/api.py
+++ stock-club-api/api.py
@@ -6,7 +6,7 @@ from bson.json_util import dumps
 
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/stock_club_db"
+app.config["MONGO_URI"] = "mongodb://stock-club:stock-club-1>@ds155213.mlab.com:55213/heroku_n7zk6r5p"
 mongo = PyMongo(app)
 api = Api(app)
 
