0c3b4625bdb11c2977bfe1939cc7ac0cfd2c5db3
johnisawesome94 <lunde367@umn.edu>
2018-11-07 14:22:07 -0600

    correct directory in serer.py

--- server.py
+++ server.py
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/stock-club'));
+app.use(express.static(__dirname + '/stock-club-console/dist/stock-club'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/stock-club/index.html'));
+res.sendFile(path.join(__dirname+'/stock-club-console/dist/stock-club/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
