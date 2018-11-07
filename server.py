c57c353389c4ce9853ba8d05e2c227bdb7af6177
johnisawesome94 <lunde367@umn.edu>
2018-11-07 14:26:13 -0600

    change spacing in server.py and change npm start

--- server.py
+++ server.py
@@ -8,8 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/stock-club-console/dist/stock-club'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/stock-club-console/dist/stock-club/index.html'));
+    res.sendFile(path.join(__dirname+'/stock-club-console/dist/stock-club/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
