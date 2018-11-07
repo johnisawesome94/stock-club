f0ec4c12f76c0d8f7717490cb3069bd0d352cad5
johnisawesome94 <lunde367@umn.edu>
2018-11-07 13:44:11 -0600

    trying

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
 from waitress import serve
-from stock-club-api.api import app
+from stock-club-api/api import app
 #serve(app, listen='0.0.0.0:5000', url_scheme='https')
 serve(app, unix_socket='/tmp/nginx.socket', url_scheme='https')
